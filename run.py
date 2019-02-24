0e7b59ddba182f26b9a485fdcf11e00dff4deb1b
Ionel Adrian Tudorascu <adrian_tudorascu@yahoo.ro>
2019-02-24 18:33:55 +0000

    created a base.html and eddited all pages to use the code from it , created careers.html

--- run.py
+++ run.py
@@ -12,7 +12,10 @@ def about():
     return render_template("about.html")
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")    
+    return render_template("contact.html")   
+@app.route('/careers')
+def careers():
+    return render_template("careers.html")    
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
